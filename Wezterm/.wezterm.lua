353c3260cf5b9e5bd36706a1373389b72036efb6
AfkaraLP <97183912+AfkaraLP@users.noreply.github.com>
2025-02-19 16:51:14 +0100

    clean up the wezterm config aswell as making it look nicer on le mac

--- Wezterm/.wezterm.lua
+++ Wezterm/.wezterm.lua
@@ -1,23 +1,35 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "tokyonight"
+-- WINDOWS SPECIFIC --
 config.default_prog = { "powershell.exe" }
-config.window_background_opacity = 0.0
-config.win32_system_backdrop = "Tabbed" -- options: Mica, 2: Acrylic, 1: Tabbed, Disable, Auto
-config.hide_tab_bar_if_only_one_tab = true
-config.window_close_confirmation = "NeverPrompt"
--- config.window_decorations = "RESIZE"
+-- WINDOWS SPECIFIC --
+
+-- MACOS SPECIFIC --
+config.window_decorations = "RESIZE"
+-- MACOS SPECIFIC --
+
+-- COLOR 01 --
+-- config.window_background_opacity = 0.7
+-- config.win32_system_backdrop = "Tabbed" -- options: Mica, 2: Acrylic, 1: Tabbed, Disable, Auto
+-- config.macos_window_background_blur = 25
+-- COLOR 01 --
+
+-- COLOR 02 --
+config.window_background_opacity = 1.0
+config.colors = { background = "#121212" }
+-- COLOR 02 --
+
+config.color_scheme = "tokyonight"
+-- config.font = wezterm.font("Monocraft")
+config.font_size = 15
 config.window_padding = { left = 2, top = 2, bottom = 2, right = 2 }
 config.window_frame = { inactive_titlebar_bg = "#000000" }
-config.font = wezterm.font("Fira Code")
-config.font_size = 10
+config.use_resize_increments = true
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+config.show_update_window = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
 
--- and finally, return the configuration to wezterm
 return config
